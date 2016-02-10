fa6c78edfb7847387d289cbf4ef34dbda5badffa
Guy Emerson <gete2@cam.ac.uk>
2016-02-10 17:02:27 +0000

    Extract situation triples from WikiWoods

---
+++ src/wikiwoods_extractcore.py
@@ -0,0 +1,75 @@
+import os, gzip, pickle
+from dmrs.core import DictPointDmrs as Dmrs, RealPred, GPred
+
+def extract(xmlstring, sits):
+    """Extract situations from a DMRS in XML form"""
+    dmrs = Dmrs.loads_xml(xmlstring)
+    # Look for verb nodes
+    for n in dmrs.iter_nodes():
+        if type(n.pred) == RealPred and n.pred.pos == 'v':
+            # Record the verb and its ARG1 and ARG2
+            output = [n.pred, None, None]
+            for i in (1,2):
+                try:  # See if the argument is there
+                    arglink = n.get_out('ARG'+str(i)).pop()
+                except KeyError:
+                    continue
+                # Get the node and the pred
+                end = dmrs[arglink.end]
+                pred = end.pred
+                if type(pred) == RealPred:
+                    # Ignore coordinations
+                    if pred.pos == 'c':
+                        continue
+                    else:
+                        output[i] = pred
+                else:
+                    # Ignore coordinations
+                    if pred == GPred('implicit_q'):
+                        continue
+                    # Record information about pronouns
+                    elif pred == GPred('pron'):
+                        pronstring = end.sortinfo['pers']
+                        try:
+                            pronstring += end.sortinfo['num']
+                        except KeyError:
+                            pass
+                        try:
+                            pronstring += end.sortinfo['gend']
+                        except KeyError:
+                            pass
+                        output[i] = pronstring
+                    else:
+                        output[i] = pred
+            # Only record the verb if one of the arguments was present
+            if output[1] or output[2]:
+                sits.append(output)
+
+# Directory of DMRSs, and directory to save triples
+SOURCE = '/usr/groups/corpora/wikiwoods-1212-tmp/dmrs/'
+TARGET = '/anfs/bigdisc/gete2/wikiwoods/core'
+
+# Process each file in SOURCE
+for filename in sorted(os.listdir(SOURCE)):
+    with gzip.open(os.path.join(SOURCE, filename),'rb') as f:
+        print(filename)
+        # List of situation triples
+        situations = []
+        # Each xml will span multiple lines,
+        # separated by an empty line
+        f.readline() # The first line is blank, for some reason
+        xml = b''
+        for line in f:
+            # Keep adding new lines until we get a blank line
+            if line != b'\n':
+                xml += line
+            else:  # Once we've found a blank line, extract the DMRS
+                extract(xml, situations)
+                xml = b''
+        # If the file does not end with a blank line:
+        if xml != b'':
+            extract(xml, situations)
+    # Save the triples in TARGET
+    newname = os.path.splitext(filename)[0]+'.pkl'
+    with open(os.path.join(TARGET, newname), 'wb') as f:
+        pickle.dump(situations, f)
