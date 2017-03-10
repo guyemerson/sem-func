e11eebaed2a31834cca9c9bcc4373390126f46ce
guyemerson <g.e.t.emerson@googlemail.com>
2017-03-10 16:54:08 +0000

    Observe links with fixed cardinality, keep raw frequencies

--- src/simplevec_link.py
+++ src/simplevec_link.py
@@ -21,6 +21,10 @@ C = int(hyp[7])
 with gzip.open(os.path.join(AUX_DIR, 'meanfield_freq_all', filename+'.pkl.gz'), 'rb') as f:
     ent = pickle.load(f)
 
+# Force cardinality
+ent /= (ent.sum(1)/C).reshape((-1, 1))
+np.clip(ent, 0, 1, ent)
+
 ### Link weights
 
 # Sum over tuples
@@ -30,25 +34,19 @@ with open(os.path.join(AUX_DIR, 'multicore-5-count_pairs.pkl'), 'rb') as f:
 
 print(len(pairs[0])+len(pairs[1]), "pairs in total")
 
-link_total = np.zeros((2, D, D))
-
-n_total = [0,0]
+link_total = np.zeros((2, D, D))  # Store sum of outer products
+n_total = np.zeros(2)  # Store number of observations
 
 # Add connections from each tuple
 for label, label_pairs in enumerate(pairs):
-    for i, ((verb, noun), n) in enumerate(label_pairs.items()):
+    for (verb, noun), n in label_pairs.items():
         link_total[label] += n * np.outer(ent[verb], ent[noun])
         n_total[label] += n
 
 # Normalise to frequencies
-link_total[0] /= n_total[0]
-link_total[1] /= n_total[1]
-
-# Take ppmi
-link_wei = np.log(link_total) - 2 * np.log(C/D)
-link_wei = link_wei.clip(0)
+link_freq = link_total / n_total.reshape((-1, 1, 1))
 
 # Save all parameters to file
 
-with open(os.path.join(AUX_DIR, 'simplevec_link', filename+'.pkl'), 'wb') as f:
-    pickle.dump(link_wei, f)
+with open(os.path.join(AUX_DIR, 'simplevec_link', filename+'-raw.pkl'), 'wb') as f:
+    pickle.dump(link_freq, f)
