10efe3b5ae23afdd606789b52152c421581e7794
Guy Emerson <gete2@cam.ac.uk>
2017-04-03 17:09:02 +0100

    Offset when accumulating links, and use multiprocessing

--- src/simplevec_link.py
+++ src/simplevec_link.py
@@ -1,8 +1,27 @@
 import os, pickle, gzip, numpy as np
+from itertools import islice
+from multiprocessing import Pool
 
 from __config__.filepath import AUX_DIR
 
-def observe_links(filename, input_dir='meanfield_all', output_dir='meanfield_link', C_index=9):
+# The following two functions are used by observe_links
+def get_work_fn(D, ent, offset):
+    "Define a work function in each worker process"
+    global work
+    def work(data):
+        "Observe a set of linked pairs"
+        link_subtotal = np.zeros((D,D))
+        n_subtotal = 0
+        for (verb, noun), n in data:
+            link_subtotal += n * (np.outer(ent[verb], ent[noun]) - offset).clip(0)
+            n_subtotal += n
+        return link_subtotal, n_subtotal
+
+def do_work(data):
+    "Use the work function that was initialised"
+    return work(data)
+
+def observe_links(filename, offset=0., input_dir='meanfield_all', output_dir='meanfield_link', C_index=9, chunk_size=100, processes=32):
     """
     Observe connections between meanfield entities, and save observed frequencies to file
     :param filename: filename without file extension
@@ -37,18 +56,27 @@ def observe_links(filename, input_dir='meanfield_all', output_dir='meanfield_lin
     link_total = np.zeros((2, D, D))  # Store sum of outer products
     n_total = np.zeros(2)  # Store number of observations
     
-    # Add connections from each tuple
+    # Multiprocessing to process tuples
+    # Chunk pairs
+    def chunk(data):
+        "Chunk a dictionary into lists of items"
+        it = iter(data)
+        for _ in range(0, len(data), chunk_size):
+            yield [(x, data[x]) for x in islice(it, chunk_size)]
+    # Spawn workers
     for label, label_pairs in enumerate(pairs):
-        for (verb, noun), n in label_pairs.items():
-            link_total[label] += n * np.outer(ent[verb], ent[noun])
-            n_total[label] += n
+        with Pool(processes, get_work_fn, (D, ent, offset)) as p:
+            for link_subtotal, n_subtotal in p.imap_unordered(do_work, chunk(label_pairs)):
+                link_total[label] += link_subtotal
+                n_total[label] += n_subtotal
     
     # Normalise to frequencies
     link_freq = link_total / n_total.reshape((-1, 1, 1))
     
     # Save all parameters to file
     
-    with gzip.open(os.path.join(AUX_DIR, output_dir, filename+'-raw.pkl.gz'), 'wb') as f:
+    fullname = filename + str(offset).replace('.','_').replace('-','~')
+    with gzip.open(os.path.join(AUX_DIR, output_dir, fullname+'-raw.pkl.gz'), 'wb') as f:
         pickle.dump(link_freq, f)
 
 if __name__ == "__main__":
@@ -56,6 +84,7 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description="Fit link weights")
     parser.add_argument('filename')
+    parser.add_argument('offset', type=float, default=0.)
     args = parser.parse_args()
     
-    observe_links(args.filename)
+    observe_links(args.filename, args.offset, output_dir='meanfield_link_offset')
