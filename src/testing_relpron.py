562a1c2295663a64cb5f03610c612716576f4408
guyemerson <g.e.t.emerson@googlemail.com>
2017-04-08 18:26:45 -0700

    Test with link weights from offset activations

--- src/testing_relpron.py
+++ src/testing_relpron.py
@@ -34,7 +34,7 @@ def cache_scores(input_dir='meanfield_relpron', output_dir='meanfield_relpron_ca
         # Get scores
         name = filename.split('.')[0]
         print(name)
-        score_fn = load_scoring_fn(name)
+        score_fn = load_scoring_fn(name, meanfield_dir=input_dir)
         scores = {t:[score_fn(t,x) for x in items] for t in terms}
         # Save scores
         with gzip.open(os.path.join(AUX_DIR, output_dir, filename), 'wb') as f:
@@ -69,7 +69,7 @@ def add_scores(scores, subdir='meanfield_relpron'):
         if tuple_name in scores:
             continue
         # Get score
-        score_fn = load_scoring_fn(string_name)
+        score_fn = load_scoring_fn(string_name, meanfield_dir=subdir)
         scores[tuple_name] = test_fn(score_fn)
         print(tuple_name)
 
@@ -127,40 +127,44 @@ def addition_score_fn(vec):
         return cosine(target, combined)
     return score_fn
 
-cache_scores()
-scores = get_scores()
-add_scores_from_cache(scores)
-save_scores(scores)
-
-#score_fn = load_baseline_scoring_fn('multicore-5-400-0-1-0-0-32-1_0-40-0_01-1_0-4_0')
-#test_fn(score_fn)
-
-
-# Ensemble
-
-addition = addition_score_fn(load_vector_model())
-
-sorted_settings = sorted(scores, key=lambda x:scores[x])
-
-from itertools import product
-
-ensemble_scores = get_scores(filename='scores_ensemble')
-
-prev_settings = None
-for settings, ratio, verb_wei, head_wei in product(sorted_settings[-20:],
-                                                   np.arange(0.06, 0.261, 0.04),
-                                                   np.arange(0.6, 1.01, 0.2),
-                                                   np.arange(0.6, 1.01, 0.2)):
-    full_settings = settings + (ratio, verb_wei, head_wei)
-    if full_settings in ensemble_scores:
-        continue
-    print(settings)
-    print(ratio, verb_wei, head_wei)
-    if prev_settings != settings:
-        semfunc = load_scoring_fn(convert_settings(settings))
-    prev_settings = settings
-    score = test_ensemble(semfunc, addition, ratio, basic_score_kwargs={'verb_weight': verb_wei, 'head_weight': head_wei})
-    print(score)
-    ensemble_scores[full_settings] = score
-
-save_scores(ensemble_scores, filename='scores_ensemble')
+if __name__ == "__main__":
+    MEANFIELD_DIR = 'meanfield_relpron_offset'
+    CACHE_DIR = 'meanfield_relpron_offset_cache_dev'
+    
+    cache_scores(MEANFIELD_DIR, CACHE_DIR)
+    scores = get_scores(MEANFIELD_DIR)
+    add_scores_from_cache(scores, MEANFIELD_DIR, CACHE_DIR)
+    save_scores(scores, MEANFIELD_DIR)
+    
+    #score_fn = load_baseline_scoring_fn('multicore-5-400-0-1-0-0-32-1_0-40-0_01-1_0-4_0')
+    #test_fn(score_fn)
+    
+    
+    # Ensemble
+    
+    addition = addition_score_fn(load_vector_model())
+    
+    sorted_settings = sorted(scores, key=lambda x:scores[x])
+    
+    from itertools import product
+    
+    ensemble_scores = get_scores(MEANFIELD_DIR, 'scores_ensemble')
+    
+    prev_settings = None
+    for settings, ratio, verb_wei, head_wei in product(sorted_settings[-1:],
+                                                       np.arange(0.18, 0.261, 0.01),
+                                                       np.arange(0.7, 1.11, 0.05),
+                                                       np.arange(0.7, 1.11, 0.05)):
+        full_settings = settings + (ratio, verb_wei, head_wei)
+        if full_settings in ensemble_scores:
+            continue
+        print(settings)
+        print(ratio, verb_wei, head_wei)
+        if prev_settings != settings:
+            semfunc = load_scoring_fn(convert_settings(settings), meanfield_dir=MEANFIELD_DIR)
+        prev_settings = settings
+        score = test_ensemble(semfunc, addition, ratio, basic_score_kwargs={'verb_weight': verb_wei, 'head_weight': head_wei})
+        print(score)
+        ensemble_scores[full_settings] = score
+    
+    save_scores(ensemble_scores, MEANFIELD_DIR, 'scores_ensemble')
